a57219216b75833632812a5aceaf8f879f8fe65a
Molefe Molefe <1858893@students.wits.ac.za>
2026-01-08 17:48:38 +0200

    Run with wandb

--- launchers/single_runs/vae/train_ae_pneumonia.sh
+++ launchers/single_runs/vae/train_ae_pneumonia.sh
@@ -22,10 +22,11 @@ export Z_CHANNELS="${1:-128}"
 export LR="2e-4"
 export KL_WEIGHT="1e-6"
 export EPOCHS="100"
-export BATCH_PER_DEVICE="4"
+export BATCH_PER_DEVICE="2"
 
 export WANDB_PROJECT="cxr-vae"
 export WANDB_RUN_GROUP="pneumonia-ae"
+export WANDB="1"
 
 # SLURM Defaults
 export SLURM_PARTITION="bigbatch"
--- launchers/single_runs/vae/train_ae_tb.sh
+++ launchers/single_runs/vae/train_ae_tb.sh
@@ -22,10 +22,10 @@ export Z_CHANNELS="${1:-128}"
 export LR="2e-4"
 export KL_WEIGHT="1e-6"
 export EPOCHS="100"
-export BATCH_PER_DEVICE="4"
+export BATCH_PER_DEVICE="2"
 export WANDB_PROJECT="cxr-vae"
 export WANDB_RUN_GROUP="tb-ae"
-
+export WANDB="1"
 # SLURM Defaults
 export SLURM_PARTITION="bigbatch"
 export SLURM_JOB_NAME="tb-ae-proto"
