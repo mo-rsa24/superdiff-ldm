813945c8105761da37580dbf4a92b3c96ee10eaa
Molefe Molefe <1858893@students.wits.ac.za>
2026-01-12 06:40:46 +0200

    Change sample batch_size

--- launchers/single_runs/ldm/train_ldm_tb.sh
+++ launchers/single_runs/ldm/train_ldm_tb.sh
@@ -27,7 +27,7 @@ export BATCH_PER_DEVICE="16"
 export EPOCHS="1500"
 export LOG_EVERY="100"
 export SAMPLE_EVERY="250"
-export SAMPLE_BATCH_SIZE="16"
+export SAMPLE_BATCH_SIZE="2"
 export LDM_CH_MULTS="1,2,4,4"
 export LDM_NUM_RES_BLOCKS="3"
 export LDM_ATTN_RES="32,16,8"
@@ -184,7 +184,7 @@ status_line "📝 Logs at" "${REPO_ROOT}/logs/${JOB_NAME}-${JOB_ID}.out"
 # TB LDM (increased capacity experiment)
 #./launchers/single_runs/ldm/train_ldm_tb.sh full_train \
 #  --batch_per_device 2 \
-#  --ae_ckpt_path runs/unified-ae-proto-increase-ae-autoencoder-1f2a36b-20260110-013819/20260110-013836/ckpts/last.flax \
-#  --ae_config_path runs/unified-ae-proto-increase-ae-autoencoder-1f2a36b-20260110-013819/20260110-013836/run_meta.json \
+#  --ae_ckpt_path /home-mscluster/mmolefe/cluster_staging/unified-ae-proto-1f2a36b_20260110-013819/runs/unified-ae-proto-increase-ae-autoencoder-1f2a36b-20260110-013819/20260110-013836/ckpts/last.flax \
+#  --ae_config_path /home-mscluster/mmolefe/cluster_staging/unified-ae-proto-1f2a36b_20260110-013819/runs/unified-ae-proto-increase-ae-autoencoder-1f2a36b-20260110-013819/20260110-013836/run_meta.json \
 #  --latent_scale_factor 0.99999905 \
 #  --wandb_project cxr-ldm-composition \
