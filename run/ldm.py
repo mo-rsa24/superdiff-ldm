b16c5ceb530a76fa2bda7b694f37827b525481f2
Molefe Molefe <1858893@students.wits.ac.za>
2026-01-14 09:13:08 +0200

    Start with num_workers=4 or 8.

--- run/ldm.py
+++ run/ldm.py
@@ -379,7 +379,7 @@ def main():
         print("INFO: Overfitting on one sample. Disabling data loader workers and shuffle.")
         loader_kwargs = dict(batch_size=batch_size, shuffle=False, num_workers=0, drop_last=True, pin_memory=True)
     else:
-        loader_kwargs = dict(batch_size=batch_size, shuffle=True, num_workers=4, drop_last=True, pin_memory=True)
+        loader_kwargs = dict(batch_size=batch_size, shuffle=True, num_workers=28, drop_last=True, pin_memory=True)
     loader = DataLoader(ds, **loader_kwargs)
 
     # --- Load Pretrained Autoencoder ---
